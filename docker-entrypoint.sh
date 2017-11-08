fbad0b5fe11ef029e6a95530ed04884fbfa43a64
klas <klas@vionlabs.com>
2017-11-08 15:55:45 +0100

    improved docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,24 @@ cmd="$@"
 if [ ${#cmd} -ge 1 ]; then
 	exec "$@"
 else
+	canonical_address=$(hostname -i)
 	run_cmd="/usr/bin/rethinkdb --bind all"
 	run_cmd="${run_cmd} -d /data"
-	run_cmd="${run_cmd} --canonical-address $(hostname -i):29015"
+	run_cmd="${run_cmd} --canonical-address ${canonical_address}:29015"
 	if [ -n "$JOIN" ]; then
 		join_resolved=$(eval "getent hosts ${JOIN}" | awk '{ print $1}')
-		if [ -n "$join_resolved" ]; then
-			run_cmd="${run_cmd} -j ${join_resolved}:29015"
-		else
-			run_cmd="${run_cmd} -j ${JOIN}:29015"
+		# ensure that we're not trying to join ourselves
+		resolved_result=""
+		for i in $join_resolved; do
+			if [ $i != $canonical_address ]; then
+				resolved_result="${resolved_result} ${i}"
+			fi
+		done
+		# ensure we're only trying to join a single IP
+		resolved_result=$(echo "$resolved_result" | awk '{ print $1 }')
+		# only add join part of command if another IP remaining
+		if [ -n "$resolved_result" ]; then
+			run_cmd="${run_cmd} -j ${resolved_result}:29015"
 		fi
 	fi
 	exec $run_cmd
